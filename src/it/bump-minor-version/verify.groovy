96ee5998fb53b02765e804e72e3578574c24e087
Erik Brudzinskis <brudzinskise@gmail.com>
2024-02-10 19:54:24 +0200

    Fix tests by committing all uncommitted changes before execution

--- src/it/bump-minor-version/verify.groovy
+++ src/it/bump-minor-version/verify.groovy
@@ -16,7 +16,7 @@
 
 def asserter = com.antonjohansson.versionlifecycleplugin.RepositoryAsserter.repository("target/it/bump-minor-version")
 
-assert asserter.commitCount == 3
+assert asserter.commitCount == 4
 assert asserter.uncommittedChanges == 0
 assert asserter.numberOfTags == 1
 assert asserter.getTagNameOf(1) == "refs/tags/v1.1.0"
